a65c5029c0adc0bde6f8ea52f9212d302c088429
fran <fran@10.0.32.235>
2019-02-11 18:50:50 +0100

    critica ready

---
+++ src/practicapoo/Critica.java
@@ -0,0 +1,62 @@
+
+package practicapoo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Critica implements Serializable{
+    //Declaracion de variables
+    private String pelicula;
+    private double puntuacion;
+    private String texto;
+    
+    //Constructores
+    
+    public Critica(){
+        pelicula ="Default";
+        puntuacion = 0.0;
+        texto = "Default";
+    }
+    public Critica(String pelicula, double puntuacion, String texto){
+        this.pelicula = pelicula;
+        
+        if(puntuacion >= 0.0 && puntuacion <= 10.0){
+            this.puntuacion = puntuacion;
+        }else{
+            this.puntuacion = 0.0;
+        }
+        
+        this.texto = texto;
+    }
+    public Critica(Critica c){
+        pelicula = c.getPelicula();
+        puntuacion = c.getPuntuacion();
+        texto = c.getTexto();
+    }
+    
+    //GET
+    public String getPelicula(){
+        return pelicula;
+    }
+    public double getPuntuacion(){
+        return puntuacion;
+    }
+    public String getTexto(){
+        return texto;
+    }
+    
+    //SET
+    public void setPelicula(String pelicula){
+        this.pelicula = pelicula;
+    }
+    public void setPuntuacion(double puntuacion){
+        if(puntuacion >= 0.0 && puntuacion <= 10.0){
+            this.puntuacion = puntuacion;
+        }else{
+            this.puntuacion = 0.0;
+        }
+    }
+    public void setTexto(String texto){
+        this.texto = texto;
+    }
+}
